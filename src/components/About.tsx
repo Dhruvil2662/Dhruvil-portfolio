0721b94aef0ba31dd7a2f6f417ea138ee4eef077
Dhruvil2662 <pateldhruvil55444@gmail.com>
2026-04-13 23:02:41 +0200

    updated about section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -7,12 +7,12 @@ const About = () => {
         <h3 className="title">About Me</h3>
         <p className="para">
           I am a results-driven professional with a Masters degree in International Marketing
-           and a strong foundation in global strategy, consumer behavior, and brand development.
-            I specialize in creating effective marketing strategies that help businesses grow and 
-            compete in dynamic markets. With expertise in digital marketing, market research, and 
-            cross-cultural communication, I turn insights into practical, impactful solutions. 
-            I am passionate about helping restaurants and service businesses enhance their branding, 
-            customer experience, and overall growth while continuously learning and innovating.
+          and a strong foundation in global strategy, consumer behavior, and brand development.
+          I specialize in creating effective marketing strategies that help businesses grow and 
+          compete in dynamic markets. With expertise in digital marketing, market research, and 
+          cross-cultural communication, I turn insights into practical, impactful solutions. 
+          I am passionate about helping restaurants and service businesses enhance their branding, 
+          customer experience, and overall growth while continuously learning and innovating.
 
         </p>
       </div>
