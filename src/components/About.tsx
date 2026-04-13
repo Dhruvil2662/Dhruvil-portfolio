53acc3dc559ff398004440a2797eafee3d9f069f
Dhruvil2662 <pateldhruvil55444@gmail.com>
2026-04-13 23:11:47 +0200

    updated about section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -6,10 +6,11 @@ const About = () => {
       <div className="about-me">
         <h3 className="title">About Me</h3>
         <p className="para">
-          
-          I am a results-driven professional with a Masters degree in International Marketing
-          and  a strong foundation in global strategy, consumer behavior, 
-          and brand development.I specialize  in creating effective marketing
+
+          I am a results-driven professional with a Masters degree in 
+          International Marketing and a strong foundation in global strategy,
+           consumer behavior, and brand development.
+           I specialize  in creating effective marketing
            strategies that help businesses grow and compete in dynamic markets.
             With expertise in digital marketing, market research, and 
           cross-cultural communication, I turn insights into practical, impactful solutions. 
